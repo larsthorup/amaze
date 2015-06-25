f44ad4534d63cce90b7e5260d9838c96ba530a93
Lars Thorup <lars@zealake.com>
2015-06-25 18:06:23 +0200

    cli test

--- test/cli/main.test.js
+++ test/cli/main.test.js
@@ -9,11 +9,36 @@
     }
     module.exports = factory.apply(this, deps);
   }
-}(['../../src/cli/main'], function (cli) {
+}(['sinon', 'lodash', '../../src/cli/main'], function (sinon, _, cli) {
 
   describe('main', function () {
 
-    it('render according to options');
+    beforeEach(function () {
+      this.sinon = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+      this.sinon.restore();
+    });
+
+    it('render according to options', function () {
+      this.sinon.stub(_, 'random').returnsArg(0); // To make the algorithm predictable for testing
+      this.sinon.stub(console, 'log');
+      cli.main({
+        rows: 2,
+        columns: 2,
+        algorithm: 'binaryTree',
+        view: 'ascii'
+      });
+      console.log.should.have.been.calledWith([
+        '+---+---+',
+        '| x     |',
+        '+---+   +',
+        '| x     |',
+        '+---+---+',
+        ''
+      ].join('\n'));
+    });
 
   });
 
