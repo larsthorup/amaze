511f9633fa268f5766644dcea62920a1067a4fe3
Lars Thorup <lars@zealake.com>
2023-12-15 16:27:43 +0100

    esm; wip

--- test/main.mjs
+++ test/main.mjs
@@ -1,5 +1,18 @@
 import '../test/mocha_setup.mjs';
 
+// const allTestFiles = [
+import '../test/model/cell.test.mjs';
+import '../test/model/grid.test.mjs';
+// '../test/model/distances.test',
+// '../test/view/ascii/grid.test',
+// '../test/view/svg/grid.test',
+// '../test/algorithm/maze/binaryTree.test',
+// '../test/algorithm/maze/sideWinder.test',
+// '../test/algorithm/maze/aldousBroder.test',
+// '../test/algorithm/maze/recursiveBacktracker.test',
+// '../test/algorithm/distance/dijkstra.test',
+// '../test/algorithm/path/longest.test',
 import '../test/wui/main.test.mjs';
+// ];
 
 window.mocha.run((failures) => { window.__mocha_failures__ = failures; });
